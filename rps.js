4d2f9f5bc0753ec5a522270dbb8f624cc704265f
domodfg <domodfg@yahoo.com.hk>
2022-07-06 20:47:13 +0800

    Change the roundresult text
    rock paper and scissor is changed to attack, magic and parry

--- rps.js
+++ rps.js
@@ -1,92 +1,91 @@
 function getRandom() {
-    return parseInt(Math.floor(Math.random() * 3));
+  return parseInt(Math.floor(Math.random() * 3));
+}
+
+function computerPlay(compChoice) {
+  if (compChoice === 0) {
+    return "attack";
+  } else if (compChoice === 1) {
+    return "magic";
+  } else {
+    return "parry";
   }
+}
 
-  function computerPlay(compChoice) {
-    if (compChoice === 0) {
-      return "ROCK";
-    } else if (compChoice === 1) {
-      return "PAPER";
-    } else {
-      return "SCISSOR";
-    }
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === "attack" && playerSelection === "parry") {
+    computerLife -= 1;
+    roundResult = `You parried the attack and break his composure! (Enemy action: ${computerSelection})`;
+  } else if (computerSelection === "attack" && playerSelection === "magic") {
+    playerLife -= 1;
+    roundResult =
+      `Your magic scythe spell is interrupted by opponent's strike! (Enemy action: ${computerSelection})`;
+  } else if (computerSelection === "parry" && playerSelection === "attack") {
+    playerLife -= 1;
+    roundResult = `Your attack get parried and you can't hold your composure! (Enemy action: ${computerSelection})`;
+  } else if (computerSelection === "parry" && playerSelection === "magic") {
+    computerLife -= 1;
+    roundResult = `Your magic scythe pierces through the shield! (Enemy action: ${computerSelection})`;
+  } else if (computerSelection === "magic" && playerSelection === "attack") {
+    computerLife -= 1;
+    roundResult = `You strike your opponent and interrupted his spell! (Enemy action: ${computerSelection})`
+  } else if (computerSelection === "magic" && playerSelection === "parry") {
+    playerLife -= 1;
+    roundResult = `You can't parry magic scythe! (Enemy action: ${computerSelection})`;
+  } else {
+    roundResult = `Draw! (Enemy action: ${computerSelection})`
   }
-
-  function playRound(playerSelection, computerSelection) {
-    if (computerSelection === "ROCK" && playerSelection === "PAPER") {
-      computerLife -= 1;
-      roundResult="you win! paper beat rock!";
-    } else if (
-      computerSelection === "PAPER" &&
-      playerSelection === "SCISSOR"
-    ) {
-      computerLife -= 1;
-      roundResult="you win! scissor beat paper!";
-    } else if (
-      computerSelection === "SCISSOR" &&
-      playerSelection === "ROCK"
-    ) {
-      computerLife -= 1;
-      roundResult="you win! rock beat scissor!";
-    } else if (computerSelection === playerSelection) {
-      roundResult="draw!";
-    } else {
-      playerLife -= 1;
-      roundResult=`you lose! ${computerSelection.toLowerCase()} beat ${playerSelection.toLowerCase()}!`;
-    }
-    results.textContent=roundResult;
-    yourLife.textContent=`Your lifes: ${playerLife}`;
-    tubaLife.textContent=`knight's lifes: ${computerLife}`;
-    if (playerLife === 0) {
-      yourLife.textContent='YOU DIED';
-    } 
-    if (computerLife === 0) {
-      tubaLife.textContent='VICTORY ACHIEVED';
-    }
+  results.textContent = roundResult;
+  yourLife.textContent = `Your lifes: ${playerLife}`;
+  tubaLife.textContent = `knight's lifes: ${computerLife}`;
+  if (playerLife <= 0) {
+    yourLife.textContent = "YOU DIED";
   }
-
-  let playerLife = 5;
-  let computerLife = 5;
-  let roundResult;
-
-  function setSelectionRock() {
-    let playerSelection = 'ROCK';
-    let compChoice = getRandom();
-    let computerSelection = computerPlay(compChoice);
-    playRound(playerSelection, computerSelection);
+  if (computerLife <= 0) {
+    tubaLife.textContent = "VICTORY ACHIEVED";
   }
+}
 
-  function setSelectionPaper() {
-    let playerSelection = 'PAPER';
-    let compChoice = getRandom();
-    let computerSelection = computerPlay(compChoice);
-    playRound(playerSelection, computerSelection);
-  }
+let playerLife = 5;
+let computerLife = 5;
+let roundResult;
 
-  function setSelectionScissor() {
-    let playerSelection = 'SCISSOR';
-    let compChoice = getRandom();
-    let computerSelection = computerPlay(compChoice);
-    playRound(playerSelection, computerSelection);
-  }
+function setSelectionAttack() {
+  let playerSelection = "attack";
+  let compChoice = getRandom();
+  let computerSelection = computerPlay(compChoice);
+  playRound(playerSelection, computerSelection);
+}
 
-  const attackbtn=document.querySelector('#attack');
-  const magicbtn=document.querySelector('#magic');
-  const parrybtn=document.querySelector('#parry');
-  const resetbtn=document.querySelector('#reset');
-  const results=document.querySelector('.move');
-  const tubaLife=document.querySelector('.tuba-life');
-  const yourLife=document.querySelector('.your-life');
-  attackbtn.addEventListener('click', setSelectionRock)
-  magicbtn.addEventListener('click', setSelectionPaper)
-  parrybtn.addEventListener('click', setSelectionScissor)
-  resetbtn.addEventListener('click', reset)
+function setSelectionMagic() {
+  let playerSelection = "magic";
+  let compChoice = getRandom();
+  let computerSelection = computerPlay(compChoice);
+  playRound(playerSelection, computerSelection);
+}
 
-  function reset() {
-    playerLife = 5;
-    computerLife = 5;
-    yourLife.textContent=`Your lifes: ${playerLife}`;
-    tubaLife.textContent=`knight's lifes: ${computerLife}`;
-  }
-  
+function setSelectionParry() {
+  let playerSelection = "parry";
+  let compChoice = getRandom();
+  let computerSelection = computerPlay(compChoice);
+  playRound(playerSelection, computerSelection);
+}
 
+const attackbtn = document.querySelector("#attack");
+const magicbtn = document.querySelector("#magic");
+const parrybtn = document.querySelector("#parry");
+const resetbtn = document.querySelector("#reset");
+const results = document.querySelector(".move");
+const tubaLife = document.querySelector(".tuba-life");
+const yourLife = document.querySelector(".your-life");
+attackbtn.addEventListener("click", setSelectionAttack);
+magicbtn.addEventListener("click", setSelectionMagic);
+parrybtn.addEventListener("click", setSelectionParry);
+resetbtn.addEventListener("click", reset);
+
+function reset() {
+  playerLife = 5;
+  computerLife = 5;
+  yourLife.textContent = `Your lifes: ${playerLife}`;
+  tubaLife.textContent = `knight's lifes: ${computerLife}`;
+}
